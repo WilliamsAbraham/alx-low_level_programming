760b650c97130c7f2da8337bc101ed1b7a8a2a02
Williams Abraham <willingtoexcel@gmail.com>
2022-09-10 05:13:16 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry point
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	char lwcase;
+
+	for (lwcase = 'a'; lwcase <= 'z'; lwcase++)
+		putchar(lwcase);
+		putchar('\n');
+
+		return (0);
+}
