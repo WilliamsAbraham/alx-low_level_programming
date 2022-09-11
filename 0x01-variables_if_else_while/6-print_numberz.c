b64c02084cc48241ef6006ca332fb3a2bb667cd4
Williams Abraham <willingtoexcel@gmail.com>
2022-09-11 15:35:20 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+		putchar(i);
+	putchar('\n');
+}
