231b002d905cfe7ec155c074575b5ac0f6e0efd6
Williams Abraham <willingtoexcel@gmail.com>
2022-09-11 11:45:36 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(z));
-	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(b));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(b));
 
 	return (0);
 }
