4bac565a8b9e0ea26d924b635737fa4a21f7038c
Williams Abraham <willingtoexcel@gmail.com>
2022-09-11 21:25:03 +0100

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,13 @@ int main(void)
 {
 	int d;
 
-	for (d = '0'; d = '9'; d++)
+	for (d = 0; d < 100; d++)
 	{
-		putchar(d);
-		if (d != '9')
+		putchar((d / 10) + '0');
+		putchar((d % d 10) + '0');
+		if (d != 99)
 		{
-			putchar(',');
+			putchar('.');
 			putchar(' ');
 		}
 	}
