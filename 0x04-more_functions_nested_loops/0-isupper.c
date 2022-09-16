589f62bbdd4682ddb954b25000316e727900d57e
Williams Abraham <willingtoexcel@gmail.com>
2022-09-16 23:32:00 +0100

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
 #include "main.h"
-
+#include <stdio.h>
 /**
+ *
  * _isupper - A function that checks for uppercase character.
  * @c: An input character
  * Return: 1 if c is uppercase or 0 otherwise
