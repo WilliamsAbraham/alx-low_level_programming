e6e6792afae41272aaccb86caa6220893d2d751b
Williams Abraham <willingtoexcel@gmail.com>
2022-09-19 23:33:58 +0100

    prints every other character starting with the first

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: An input string
+ * Return: Nothing
+ */
+void puts2(char *strn)
+{
+	int lent = 0, i = 0;
+
+	while (strn[lent] != '\0')
+		lent++;
+
+	lent -= 1;
+
+	for (; i <= lent; i += 2)
+		_putchar(strn[i]);
+
+	_putchar('\n');
+}
