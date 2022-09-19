05c2b2f73103022db05b30d97930cefa1d907b59
Williams Abraham <willingtoexcel@gmail.com>
2022-09-19 23:36:13 +0100

    prints every other character starting with the first

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 /**
  * puts2 - a function that prints every other character of a string,
  * starting with the first character, followed by a new line.
- * @str: An input string
+ * @strn: An input string
  * Return: Nothing
  */
 void puts2(char *strn)
